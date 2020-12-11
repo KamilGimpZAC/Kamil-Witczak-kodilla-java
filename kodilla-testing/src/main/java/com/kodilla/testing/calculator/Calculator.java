fa97355673eaede246531b3c46ca0b483ba892eb
Kamil Witczak <kamilgimp99@gmail.com>
2020-12-11 16:48:41 +0100

    6.2 added new Calculator class and testing

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add() {
+        return a + b;
+    }
+
+    public int subtract() {
+        if (a > b)
+            return a - b;
+
+        else
+            return b - a;
+    }
+}
