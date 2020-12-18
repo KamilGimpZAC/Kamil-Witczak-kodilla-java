68cb22f3bee90137ebf18202f368c1c167039305
Kamil Witczak <kamilgimp99@gmail.com>
2020-12-18 13:41:30 +0100

    6.6 corrected metod listBooksInHandsOf

--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -21,14 +21,6 @@ public class BookLibrary {
     }
 
     public List<Book> listBooksInHandsOf(LibraryUser libraryUser){
-        List<Book> resultList = libraryDatabase.listBooksInHandsOf(libraryUser);
-        if(libraryUser.equals(new LibraryUser("Tom", "Hanks", "12345"))){
-                return resultList;
-        }
-        if(libraryUser.equals(new LibraryUser("David", "Morse", "12345"))){
-            return  resultList;
-        }
-        List<Book> outputBooks = resultList;
-        return  outputBooks;
+        return libraryDatabase.listBooksInHandsOf(libraryUser);
     }
 }
