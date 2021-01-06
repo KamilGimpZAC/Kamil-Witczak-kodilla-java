0a55ef86b9975a99baa79c602131970457d70693
Kamil Witczak <kamilgimp99@gmail.com>
2021-01-06 17:01:09 +0100

    7.6 summing only one task

--- kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
@@ -84,4 +84,12 @@ public class Country {
     public List<Country> getCountries(){
         return countries;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+        return Objects.equals(countryName, country.countryName);
+    }
 }
