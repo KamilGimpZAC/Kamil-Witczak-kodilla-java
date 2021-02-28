ed18e94098ff97e14245e3a0812c6fbc84027837
Kamil Witczak <kamilgimp99@gmail.com>
2021-02-28 16:16:10 +0100

    16.6 added

--- kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -2,6 +2,9 @@ package com.kodilla.stream.invoice.simple;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleInvoiceTestSuite {
@@ -15,8 +18,17 @@ class SimpleInvoiceTestSuite {
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 1", 17.28), 2.0));
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5));
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 3",  6.49), 5.0));
-
         //Then
         assertEquals(108.975, invoice.getValueToPay(), 0.001);
+    }
+
+    @Test
+    void testDebbuger(){
+        //Given
+        SimpleItem simpleProduct2 = new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5);
+        //When
+        simpleProduct2.getValue();
+        //Then
+        //do nothing
     }
 }
